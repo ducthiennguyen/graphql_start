4de2ba1dfafc1da98eb75fc16c88f6b0675e6343
ducthiennguyen <nguyenducthien23071999@gmail.com>
2020-02-04 09:43:38 +0900

    new client

--- server/app.js
+++ server/app.js
@@ -13,11 +13,23 @@ mongoose.connection.once('open', () => {
   console.log('Connected to database');
 });
 
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://thien:ndt12321@graphqlstart-70zq3.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   console.log(err);
+//   client.close();
+// });
+
+
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }));
 
-app.listen(5000, () => {
-  console.log('Listening on port 5000')}
+app.listen(4000, () => {
+  console.log('Listening on port 4000')}
 );
